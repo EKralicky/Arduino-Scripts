f002e33aa05080f45e47959f2116dd559e527536
louis dispencer <92549672+OctorockEater99@users.noreply.github.com>
2023-11-06 19:52:04 -0500

    Add files via upload

---
+++ Scripts/SumoBot/state_machine.h
@@ -0,0 +1,21 @@
+#ifndef STATE_MACHINE
+#define STATE_MACHINE
+
+typedef enum {
+  START,
+  SEARCH,
+  ATTACK,
+  STOP
+} stateCodes;
+
+typedef enum {
+  PASS,
+  FAIL
+} stateReturnCodes;
+
+typedef struct {
+  stateCodes source;
+  stateReturnCodes output;
+  stateCodes dest;
+} transition;
+
